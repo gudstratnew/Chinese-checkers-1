2e5a8d563b12a91830aeecafbba51acedbe7be00
pokemonsta433 <wbgisaac@gmail.com>
2022-04-10 03:53:30 -0400

    Made learning for the opening! <-- might wanna increase opening depth to at least 9 after I generate the opening tablebase overnight

--- test.py
+++ test.py
@@ -5,12 +5,13 @@ import TwoPlayers
 import ThreePlayers
 import FourPlayers
 import SixPlayers
-import os
 
+#import os
 #os.chdir("./Final Project/Chinese-checkers-1")
 
 #ai: ai player
 #h: human player
+
 TwoPlayers.TwoPlayers("ai", "ai")
 
 #ThreePlayers.TreePlayers("ai", "ai", "ai")
